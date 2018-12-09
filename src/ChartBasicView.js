9115cb710e213d1e85541c763bb12c59f69624f3
talkrz <krzysztof.talajko@gmail.com>
2018-12-09 02:45:41 +0100

    Update to newest version of price-chart

--- src/ChartBasicView.js
+++ src/ChartBasicView.js
@@ -17,8 +17,20 @@ const style = {
   fontSize: 14,
   padding: 5,
   margin: [10, 10, 10, 10],
-  candlestickHeight: 0.8,
-  scaleWidth: 38,
+  geometry: {
+    boxPrice: {
+      height: 0.8,
+      top: 0,
+      padding: 5,
+      margin: [10, 48, 10, 10],
+    },
+    boxVolume: {
+      height: 0.2,
+      top: 0.8,
+      padding: 5,
+      margin: [10, 48, 10, 10],
+    }
+  },
 };
 
 export default function ChartBasicView({ parentSetTheme }) {
--- src/ChartView.js
+++ src/ChartView.js
@@ -18,8 +18,20 @@ const styles = {
     fontSize: 14,
     padding: 5,
     margin: [10, 10, 10, 10],
-    candlestickHeight: 0.8,
-    scaleWidth: 38,
+    geometry: {
+      boxPrice: {
+        height: 0.8,
+        top: 0,
+        padding: 5,
+        margin: [10, 48, 10, 10],
+      },
+      boxVolume: {
+        height: 0.2,
+        top: 0.8,
+        padding: 5,
+        margin: [10, 48, 10, 10],
+      }
+    },
   },
   dark: {
     stickMargin: 2,
@@ -35,8 +47,20 @@ const styles = {
     fontSize: 14,
     padding: 5,
     margin: [10, 10, 10, 10],
-    candlestickHeight: 0.8,
-    scaleWidth: 38,
+    geometry: {
+      boxPrice: {
+        height: 0.8,
+        top: 0,
+        padding: 5,
+        margin: [10, 10, 10, 10],
+      },
+      boxVolume: {
+        height: 0.2,
+        top: 0.8,
+        padding: 5,
+        margin: [10, 10, 10, 10],
+      }
+    },
   },
 }
 
