1396d3769248554303a8acd99aed4f0ffa5ad25e
talkrz <krzysztof.talajko@gmail.com>
2018-12-19 22:40:16 +0100

    Updates

---
+++ precache-manifest.bebcf5a0d8d9b6b4ff92ddc669cf1b88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dccd3c2b768b05b8e5d3",
+    "url": "/price-chart-demo/static/js/runtime~main.dccd3c2b.js"
+  },
+  {
+    "revision": "29582f124e577e98e9b8",
+    "url": "/price-chart-demo/static/js/main.29582f12.chunk.js"
+  },
+  {
+    "revision": "78fa1d7f359a8e9073d8",
+    "url": "/price-chart-demo/static/js/1.78fa1d7f.chunk.js"
+  },
+  {
+    "revision": "29582f124e577e98e9b8",
+    "url": "/price-chart-demo/static/css/main.fbf94484.chunk.css"
+  },
+  {
+    "revision": "2989a9be7f959390b10e6b62a89652ff",
+    "url": "/price-chart-demo/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/price-chart-demo/precache-manifest.ad61647c9532a5e2d19e769b4839ccc1.js"
+  "/price-chart-demo/precache-manifest.bebcf5a0d8d9b6b4ff92ddc669cf1b88.js"
 );
 
 workbox.clientsClaim();
