ff4043e55c20d208660acd9c6187be136df8a626
talkrz <krzysztof.talajko@gmail.com>
2018-12-19 22:37:49 +0100

    Add pure JS demo

--- src/App.js
+++ src/App.js
@@ -14,17 +14,18 @@ export default function App() {
       <header className={`App-header App-header-${theme}`}>
         Price chart demo
         <button
-          className={`App-header-button App-header-button-${theme}`}
+          className="App-header-button"
           onClick={() => setView('basic')}
         >
           basic
         </button>
         <button
-          className={`App-header-button App-header-button-${theme}`}
+          className="App-header-button"
           onClick={() => setView('advanced')}
         >
           advanced
         </button>
+        <a href="/pureJs.html" target="_blank">pure Javascript</a>
       </header>
       <div className="App-content">
         {view === 'advanced' && <ChartView parentSetTheme={setTheme} />}
